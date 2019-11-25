78820752b3f7c8a49b8cc3624381fa6b427afa95
super-model-4696 <jtb6179@g.rit.edu>
2019-11-25 22:11:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,12 +17,12 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  music_I_love = ["Avenged Sevenfold", "Tool", "My Chemical Romance", "Alice in Chains"]
+  music_I_love = ["Avenged Sevenfold", "Tool", "My Chemical Romance", "arrays!"]
   x = music_I_love.pop
 end
 
 def remove_element_from_start_of_array(array)
-   music_I_love = ["Panic at the disco","Avenged Sevenfold", "Tool", "My Chemical Romance", "Alice in Chains"]
+   music_I_love = ["wow","Avenged Sevenfold", "Tool", "My Chemical Romance", "Alice in Chains"]
    t = music_I_love.shift 
 end
 
@@ -38,7 +38,7 @@ end
 
 def retrieve_last_element_from_array(array)
    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-   arrays[-1]
+   array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
