8a79e240ee384572cdee4eafa8fbc5ed1feac587
super-model-4696 <jtb6179@g.rit.edu>
2019-11-25 21:59:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,11 +13,12 @@ end
 
 def add_element_to_start_of_array(array, element)
   music_I_love = ["Avenged Sevenfold", "Tool", "My Chemical Romance", "Alice in Chains"]
-  music_I_love.unshift("")
+  music_I_love.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  music_I_love = ["Avenged Sevenfold", "Tool", "My Chemical Romance", "Alice in Chains"]
+  music_I_love.pop
 end
 
 def remove_element_from_start_of_array(array)
