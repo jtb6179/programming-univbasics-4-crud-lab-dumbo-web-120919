eb8c9770658b6841c218880f84642f98bbb96fc6
super-model-4696 <jtb6179@g.rit.edu>
2019-11-25 22:03:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+   music_I_love = ["Avenged Sevenfold", "Tool", "My Chemical Romance", "Alice in Chains"]
 end
 
 def retrieve_first_element_from_array(array)
